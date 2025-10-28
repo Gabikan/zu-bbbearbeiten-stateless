4dad96c909979e5c3ce80f7595b75a13c2cf6fd0
Gabriel <142885804+Gabikan@users.noreply.github.com>
2025-10-28 16:52:05 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,26 +1,23 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 items = []
-
 
 @dataclass
 class Item:
     text: str
+    date: datetime = None
     isCompleted: bool = False
 
-
-def add(text):
+def add(text, date=None):
     text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
-
+    items.append(Item(text, date))
 
 def get_all():
     return items
 
-
 def get(index):
     return items[index]
-
 
 def update(index):
     items[index].isCompleted = not items[index].isCompleted
